4f3d383d6a763e78ce268d88e5ca9f7364494f04
laliiiiiiiiiii <lara.silva.pires@escola.pr.gov.br>
2025-10-08 11:54:38 -0300

    Update and rename LICENSE to palavrasruins.js

---
+++ palavrasruins.js
@@ -0,0 +1,8 @@
+// Usamos 'var' para garantir que esta variável seja globalmente acessível ao 'script.js'
+var stopWords = [
+    'o', 'a', 'os', 'as', 'de', 'do', 'da', 'dos', 'das', 'e', 'é', 'em', 'um', 'uma',
+    'uns', 'umas', 'para', 'com', 'sem', 'por', 'no', 'na', 'nos', 'nas', 'meu', 'minha',
+    'seu', 'sua', 'mas', 'ou', 'se', 'ao', 'aos', 'à', 'às', 'que', 'qual', 'quem', 
+    'onde', 'isso', 'este', 'esta', 'isto', 'eu', 'tu', 'ele', 'nós', 'vós', 'eles',
+    'me', 'te', 'lhe', 'nos', 'vos', 'lhes', 'muito', 'pouco', 'também', 'apenas', 'ser'
+];
